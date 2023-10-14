c102c161afc0934cbf6d5c4fae88e53925b60736
Peter Bishop <pjb.den@gmail.com>
2023-10-14 01:17:45 -0600

    dm chat from both users confirmed working

--- src/components/Rooms_DM.js
+++ src/components/Rooms_DM.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/esm/Row";
-import ChatComponent from "../models/chat";
+import Card from "react-bootstrap/esm/Card";
 
 export default function Rooms_DM() {
   const { _id } = useParams();
   const thisUser = localStorage.getItem("_id");
+  const navigate = useNavigate();
+  const [alt, setAlt] = useState(false);
   const [resp, setResp] = useState([
     {
       _id: "",
@@ -29,6 +30,7 @@ export default function Rooms_DM() {
   const [success, setSuccess] = useState(false);
   const [update, setUpdate] = useState(false);
   const dmRoomName = _id + thisUser;
+  const dmRoomNameAlt = thisUser + _id;
 
   const [message, setMessage] = useState({
     content: "",
@@ -43,6 +45,9 @@ export default function Rooms_DM() {
     button: {
       width: "10vw",
       margin: "10px",
+    },
+    sender: {
+      textAlign: "right",
     },
   };
 
@@ -63,8 +68,27 @@ export default function Rooms_DM() {
         }
       })
       .catch((error) => {
-        setSuccess(false);
         setResp(error);
+        axios
+          .get(
+            `http://localhost:4200/rooms/${dmRoomNameAlt}`,
+
+            {
+              responseType: "json",
+            }
+          )
+          .then((response) => {
+            setResp(response.data);
+            setDisplay(response.data.messages);
+            if (response.status === 200) {
+              setSuccess(true);
+              setAlt(true);
+            }
+          })
+          .catch((error) => {
+            setSuccess(false);
+            setResp(error);
+          });
       });
 
     axios
@@ -110,7 +134,7 @@ export default function Rooms_DM() {
       .post(
         `http://localhost:4200/rooms/add`,
         {
-          name: _id + thisUser,
+          name: alt ? thisUser + _id : _id + thisUser,
           members: [_id, thisUser],
         },
 
@@ -136,9 +160,14 @@ export default function Rooms_DM() {
     e.preventDefault();
 
     axios
-      .put(`http://localhost:4200/rooms/${dmRoomName}/message/${thisUser}`, {
-        content: message.content,
-      })
+      .put(
+        alt
+          ? `http://localhost:4200/rooms/${dmRoomNameAlt}/message/${thisUser}`
+          : `http://localhost:4200/rooms/${dmRoomName}/message/${thisUser}`,
+        {
+          content: message.content,
+        }
+      )
       .catch((error) => {
         return (
           <Container>
@@ -166,26 +195,27 @@ export default function Rooms_DM() {
 
   return (
     <Container>
-      <h1>Direct Chat</h1>
-      {/* <h2>{_id} &</h2>
-      <h4>{user1.username}</h4>
-      <h2>{thisUser}</h2>
-      <h4>{user2.username}</h4> */}
-      {display.map(({ user, content }) => {
-        return user === thisUser ? (
-          <Container>
-            <h4>
-              {user2.username}: {content}
-            </h4>
-          </Container>
-        ) : (
-          <Container>
-            <h4>
-              {user1.username}: {content}
-            </h4>
-          </Container>
-        );
-      })}
+      <h1 style={{ textAlign: "center" }}>Direct Chat</h1>
+
+      <h4 style={{ textAlign: "center" }}>Between you and {user1.username}</h4>
+
+      <Card>
+        {display.map(({ user, content }) => {
+          return user === thisUser ? (
+            <Container>
+              <h4 style={style.sender}>
+                {content} :{user2.username}
+              </h4>
+            </Container>
+          ) : (
+            <Container>
+              <h4>
+                {user1.username}: {content}
+              </h4>
+            </Container>
+          );
+        })}
+      </Card>
       <Form onSubmit={handleSubmit} style={style.form}>
         <Form.Group controlId="name">
           <Form.Label>Message</Form.Label>
@@ -198,6 +228,14 @@ export default function Rooms_DM() {
             required
           />
         </Form.Group>
+        <Button
+          variant="dark"
+          onClick={() => {
+            navigate("/users/all");
+          }}
+        >
+          BACK
+        </Button>
         <Button variant="dark" type="submit" style={style.button}>
           SEND
         </Button>
